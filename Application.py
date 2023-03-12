262c26ae54543578b8de517fe49fc267036bf8e9
B1MOLE <125443237+B1MOLE@users.noreply.github.com>
2023-03-12 23:18:42 +0300

    Update Application.py

--- Application.py
+++ Application.py
@@ -1,77 +1,60 @@
-import tkinter.messagebox as messagebox
 import tkinter as tk
-from tkinter import Menu, filedialog
+from tkinter import filedialog
 import pandas as pd
 import os
 import Model
 
+class Main(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Классификатор")
+        self.geometry("500x300")
 
-class Application:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("URL Classifier")             # Название окна
-        self.master.configure(background='#F0F0F0')     # Цвет фона
+        self.label = tk.Label(self, text="Классификатор интернет ресурсов", bg='#F0F0F0', fg='#333333',
+                              font=('Arial', 18, 'bold'))
+        self.label.pack(pady=(20, 10))
 
+        self.button = tk.Button(self, text="Классификатор", command=self.open_classificator, font=('Arial', 12))
+        self.button.pack(pady=10)
+
+        self.button1 = tk.Button(self, text="Классифицированные ссылки", command=self.open_classified_urls,
+                                 font=('Arial', 12))
+        self.button1.pack(pady=10)
+
+        self.button2 = tk.Button(self, text="Выход", command=self.destroy, font=('Arial', 12))
+        self.button2.pack(pady=10)
+
+    def open_classificator(self):
+        self.withdraw()
+        Classificator(self)
+
+    def open_classified_urls(self):
+        self.withdraw()
+        Classified_urls(self)
+
+
+class Classificator(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self, *args, **kwargs)
+        self.title("Классификатор")
+        self.geometry("600x150")
         self.model = Model.Model("model", "vect")
 
-        # Создание меню
-        menu_bar = Menu(self.master)
-        self.master.config(menu=menu_bar)
+        self.label1 = tk.Label(self, text='Введите URL для классификации', font=('Arial', 12))
+        self.label1.grid(row=0, column=0, padx=10, pady=10)
 
-        # Создание подменю "File"
-        file_menu = Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="Classify URL", command=self.classify_url)
-        file_menu.add_command(label="Clear History", command=self.clear_history)
-        file_menu.add_command(label="Classify URLs from CSV", command=self.classify_urls_from_csv)
-        file_menu.add_command(label="Exit", command=self.master.quit)
-        menu_bar.add_cascade(label="File", menu=file_menu)
+        self.url_entry = tk.Entry(self, width=20, font=('Arial', 12))
+        self.url_entry.grid(row=0, column=1, padx=10, pady=10)
 
-        # Создание заголовка приложения
-        self.title_label = tk.Label(self.master, text="URL Classifier", bg='#F0F0F0', fg='#333333',
-                                    font=('Arial', 24, 'bold'))
-        self.title_label.pack(pady=(20, 10))
+        self.button1 = tk.Button(self, text='Классифицировать', command=self.classify_url, font=('Arial', 12))
+        self.button1.grid(row=1, column=1, padx=10, pady=10)
 
-        # Создание фрейма для UI-элементов
-        self.frame = tk.Frame(master, bg='#F0F0F0', pady=20, padx=20)
-        self.frame.pack()
+        self.button2 = tk.Button(self, text='Классифицировать ссылки из CSV файла', command=self.classify_urls_from_csv,
+                                 font=('Arial', 12))
+        self.button2.grid(row=1, column=0, padx=10, pady=10)
 
-        # Создание метки для ввода URL
-        self.url_label = tk.Label(self.frame, text="Enter a URL:", bg='#F0F0F0', fg='#333333',
-                                  font=('Arial', 16))
-        self.url_label.pack(pady=(10, 5))
-
-        # Создание поля для ввода URL
-        self.url_entry = tk.Entry(self.frame, width=40, font=('Arial', 16))
-        self.url_entry.pack()
-
-        # Создание кнопки "Classify URL"
-        self.classify_button = tk.Button(self.frame, text="Classify URL", command=self.classify_url, bg='#B7D1E2',
-                                         activebackground='#8FC2E5', fg='#333333', activeforeground='#333333',
-                                         font=('Arial', 16, 'bold'))
-        self.classify_button.pack(pady=(20, 10))
-
-        # Создание кнопки "Classify URLs from CSV"
-        self.classify_button_csv = tk.Button(self.frame, text="Classify URLs from CSV", command=self.classify_urls_from_csv, bg='#B7D1E2',
-                                         activebackground='#8FC2E5', fg='#333333', activeforeground='#333333',
-                                         font=('Arial', 16, 'bold'))
-        self.classify_button_csv.pack(pady=(5, 10))
-
-        # создание таблицу для отображения классифицированных URL-адресов
-        self.table_frame = tk.Frame(self.frame, bg='#F0F0F0', bd=2, relief=tk.GROOVE)
-        self.table_frame.pack(pady=(0, 20))
-
-        self.table_label = tk.Label(self.table_frame, text="History of Classified URLs", bg='#F0F0F0', fg='#333333',
-                                    font=('Arial', 12, 'bold'))
-        self.table_label.pack()
-
-        self.table = tk.Label(self.table_frame, text="", bg='#F0F0F0', fg='#333333', font=('Arial', 12), justify='left')
-        self.table.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-        # Создание кнопки "Clear history"
-        self.clear_button = tk.Button(self.frame, text="Clear History", command=self.clear_history, bg='#B7D1E2',
-                                      activebackground='#8FC2E5', fg='#333333', activeforeground='#333333',
-                                      font=('Arial', 12, 'bold'))
-        self.clear_button.pack(pady=(20, 0))
+        self.button3 = tk.Button(self, text='Назад', command=self.back, font=('Arial', 12))
+        self.button3.place(x=300, y=100)
 
     def classify_url(self):
         url = self.url_entry.get()
@@ -85,7 +68,7 @@ class Application:
 
         # Обновление таблицы с классифицированными URL-адресами
         classified_urls = pd.read_csv('classified_urls.csv')
-        self.table.configure(text=classified_urls.to_string(index=False))
+        tk.messagebox.showinfo(title="Классификация URL", message="Класс этой ссылки: " + " " + category)
 
     def classify_urls_from_csv(self):
         csv_file_path = filedialog.askopenfilename(title="Select CSV file")
@@ -103,7 +86,45 @@ class Application:
 
             # Обновление таблицы с классифицированными URL-адресами
             classified_urls = pd.read_csv('classified_urls.csv')
-            self.table.configure(text=classified_urls.to_string(index=False))
+            tk.messagebox.showinfo(title="Классификация URL", message="Ссылки успешно определены")
+
+    def back(self):
+        self.destroy()
+        app = Main()
+        app.grab_set()
+
+class Classified_urls(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        if os.path.exists("classified_urls.csv"):
+            tk.Toplevel.__init__(self, *args, **kwargs)
+            self.classified_urls = pd.read_csv('classified_urls.csv')
+            self.title("Классифицированные ссылки")
+            self.geometry("500x400")
+            self.model = Model.Model("model", "vect")
+
+            # создание таблицы для отображения классифицированных URL-адресов
+            self.table_frame = tk.Frame(self, bg='#F0F0F0', bd=2, relief=tk.GROOVE)
+            self.table_frame.pack(pady=(10, 20))
+
+            self.table_label = tk.Label(self.table_frame, text="История классифицированных ссылок", bg='#F0F0F0', fg='#333333',
+                                        font=('Arial', 12, 'bold'))
+            self.table_label.pack()
+
+            self.table = tk.Label(self.table_frame, text="", bg='#F0F0F0', fg='#333333', font=('Arial', 12), justify='left')
+            self.table.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+            self.table.configure(text=self.classified_urls.to_string(index=False, max_colwidth=30))
+
+            # Создание кнопки "Clear history"
+            self.clear_button = tk.Button(self, text="Очистить историю", command=self.clear_history, bg='white',activebackground='#8FC2E5', fg='#333333', activeforeground='#333333',font=('Arial', 12, 'bold'))
+            self.clear_button.place(x=160, y=250)
+
+            # Создание кнопки "Назад"
+            self.back_button = tk.Button(self, text="Назад", command=self.back, bg='white',activebackground='#8FC2E5', fg='#333333', activeforeground='#333333',font=('Arial', 12, 'bold'))
+            self.back_button.place(x=210, y=300)
+        else:
+            tk.messagebox.showerror(title="Error", message="Файл пустой")
+            app = Main()
+            app.grab_set()
 
     def clear_history(self):
         # Очистка истории классифицированных URL- адресов
@@ -113,11 +134,14 @@ class Application:
         except OSError:
             tk.messagebox.showerror(title="Error", message="An error occurred while attempting to clear the history.")
 
+    def back(self):
+        self.destroy()
+        app = Main()
+        app.grab_set()
 
-if __name__ == '__main__':
-    # Создание окна
-    root = tk.Tk()
-    app = Application(root)
-    root.mainloop()
 
+
+if __name__ == "__main__":
+     app = Main()
+     app.mainloop()
 
