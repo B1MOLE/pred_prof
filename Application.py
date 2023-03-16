e58fcf7cdf500cbfdad08c10d5976b943ea3129e
B1MOLE <125443237+B1MOLE@users.noreply.github.com>
2023-03-16 23:19:38 +0300

    Update Application.py

--- Application.py
+++ Application.py
@@ -15,18 +15,17 @@ class Main(tk.Tk):
         self.geometry("500x300+{}+{}".format(x, y))
 
         self.label = tk.Label(self, text="Классификатор интернет ресурсов", bg='#F0F0F0', fg='#333333',
-                              font=('Arial', 18, 'bold'))
-        self.label.pack(pady=(20, 10))
+                              font=('Arial', 20, 'bold'))
+        self.label.place(x=10, y=10)
 
-        self.button = tk.Button(self, text="Классификатор", command=self.open_classificator, font=('Arial', 12))
-        self.button.pack(pady=10)
+        self.button = tk.Button(self, text="Классификатор", command=self.open_classificator, font=('Arial', 16), width=26, height=2)
+        self.button.place(x=90, y=60)
 
-        self.button1 = tk.Button(self, text="Классифицированные ссылки", command=self.open_classified_urls,
-                                 font=('Arial', 12))
-        self.button1.pack(pady=10)
+        self.button1 = tk.Button(self, text="Классифицированные ссылки", command=self.open_classified_urls,font=('Arial', 16), width=26, height=2)
+        self.button1.place(x=90, y=140)
 
-        self.button2 = tk.Button(self, text="Выход", command=self.destroy, font=('Arial', 12))
-        self.button2.pack(pady=10)
+        self.button2 = tk.Button(self, text="Выход", command=self.destroy, font=('Arial', 16), width=26, height=2)
+        self.button2.place(x=90, y=220)
 
     def open_classificator(self):
         self.withdraw()
@@ -48,27 +47,26 @@ class Classificator(tk.Toplevel):
         self.geometry("500x300+{}+{}".format(x, y))
         self.model = Model.Model("model", "vect")
 
-        self.label1 = tk.Label(self, text='Введите URL для классификации', font=('Arial', 12))
-        self.label1.place(x=125, y=30)
+        self.label1 = tk.Label(self, text='Введите URL для классификации:', font=('Arial', 20), width=30, height=2)
+        self.label1.place(x=10, y=20)
 
-        self.url_entry = tk.Entry(self, width=20, font=('Arial', 12))
-        self.url_entry.place(x=150, y=80)
+        self.url_entry = tk.Entry(self, width=28, font=('Arial', 20))
+        self.url_entry.place(x=37, y=80)
 
-        self.button1 = tk.Button(self, text='Классифицировать', command=self.classify_url, font=('Arial', 12))
-        self.button1.place(x=165, y=130)
+        self.button1 = tk.Button(self, text='Классифицировать', command=self.classify_url, font=('Arial', 15), width=20, height=2)
+        self.button1.place(x=35, y=200)
 
-        self.button2 = tk.Button(self, text='Классифицировать ссылки из CSV файла', command=self.classify_urls_from_csv,
-                                 font=('Arial', 12))
-        self.button2.place(x=90, y=180)
+        self.button2 = tk.Button(self, text='Классифицировать ссылки из CSV файла', command=self.classify_urls_from_csv,font=('Arial', 15), width=38, height=2)
+        self.button2.place(x=35, y=125)
 
-        self.button3 = tk.Button(self, text='Назад', command=self.back, font=('Arial', 12))
-        self.button3.place(x=210, y=230)
+        self.button3 = tk.Button(self, text='Назад', command=self.back, font=('Arial', 15), width=15, height=2)
+        self.button3.place(x=285, y=200)
 
     def classify_url(self):
         url = self.url_entry.get()
         category = self.model.predict_class(url)
         probability = self.model.predict_probability(url)
-        probability = (probability * 100)
+        probability = round(probability * 100)
 
         # Сохранение URL и категорию в файл CSV
         df = pd.DataFrame({'URL': [url], 'Category': [category], 'Probability': [probability]})
@@ -78,7 +76,7 @@ class Classificator(tk.Toplevel):
 
         # Обновление таблицы с классифицированными URL-адресами
         classified_urls = pd.read_csv('classified_urls.csv')
-        tk.messagebox.showinfo(title="Классификация URL", message="Класс этой ссылки: " + " " + category + " " + "с точностью" + " " + str(probability) + "%")
+        tk.messagebox.showinfo(title="Классификация URL", message="Класс этой ссылки: " + " " + category + " " + "с вероятностью" + " " + str(probability) + "%")
 
     def classify_urls_from_csv(self):
         csv_file_path = filedialog.askopenfilename(title="Select CSV file")
@@ -89,7 +87,7 @@ class Classificator(tk.Toplevel):
                 url = row[0]
                 category = self.model.predict_class(url)
                 probability = self.model.predict_probability(url)
-                probability = (probability * 100)
+                probability = round(probability * 100)
                 data.append((url, category, probability))
 
             # Сохранение URL и категорию в файл CSV
@@ -122,37 +120,41 @@ class Classified_urls(tk.Toplevel):
             self.table_frame = tk.Frame(self, bg='#F0F0F0', bd=1, relief=tk.GROOVE)
             self.table_frame.pack(pady=(1, 1))
 
-            self.table_label = tk.Label(self.table_frame, text="История классифицированных ссылок", bg='#F0F0F0', fg='#333333',
+            self.table_label = tk.Label(self.table_frame, text="История классифицированных ссылок", bg='#F0F0F0',
+                                        fg='#333333',
                                         font=('Arial', 12, 'bold'))
             self.table_label.pack()
 
             self.table_scrollbar = tk.Scrollbar(self.table_frame)
             self.table_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-            self.table = tk.Text(self.table_frame, bg='#F0F0F0', fg='#333333', font=('Arial', 12), wrap=tk.WORD, yscrollcommand=self.table_scrollbar.set, height=7)
+            self.table = tk.Text(self.table_frame, bg='#F0F0F0', fg='#333333', font=('Arial', 12), wrap=tk.CHAR, yscrollcommand=self.table_scrollbar.set, height=11, width=50)
             self.table.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
             self.table.configure(state=tk.NORMAL)
-            self.table.insert(tk.END, self.classified_urls.to_string(index=False, max_colwidth=30))
+
+            # Вставка заголовков таблицы
+            self.table.insert(tk.END, '{:36} {:36} {:36}\n'.format("Ссылка", "Класс", "Вероятность"),
+                              ('col1', 'col2', 'col3'))
+
+            # Вставка разделительной строки
+            self.table.insert(tk.END, '-' * 90 + '\n')
+
+            # Вставка содержимого таблицы в виде строк
+            for row in self.classified_urls.to_numpy():
+                row = [str(cell)[:15] + '...' if len(str(cell)) > 15 else str(cell) for cell in row]
+                self.table.insert(tk.END, '{:37} {:40} {:37}\n'.format(row[0], row[1], row[2]), ('col1', 'col2', 'col3'))
+
             self.table.configure(state=tk.DISABLED)
 
             self.table_scrollbar.config(command=self.table.yview)
 
-            # Установка выравнивания текста в столбцах
-            self.table.tag_configure('col1', justify='left')
-            self.table.tag_configure('col2', justify='center')
-            self.table.tag_configure('col3', justify='right')
-
-            # Вставка содержимого таблицы в виде строк
-            for row in self.classified_urls.to_numpy():
-                self.table.insert(tk.END, '{:<30} {:^30} {:>30}\n'.format(*row), ('col1', 'col2', 'col3'))
-
             # Создание кнопки "Clear history"
-            self.clear_button = tk.Button(self, text="Очистить историю", command=self.clear_history, font=('Arial', 12))
-            self.clear_button.place(x=170, y=180)
+            self.clear_button = tk.Button(self, text="Очистить историю", command=self.clear_history, font=('Arial', 13), width=25, height=2)
+            self.clear_button.place(x=10, y=240)
 
             # Создание кнопки "Назад"
-            self.button3 = tk.Button(self, text='Назад', command=self.back, font=('Arial', 12))
-            self.button3.place(x=210, y=230)
+            self.button3 = tk.Button(self, text='Назад', command=self.back, font=('Arial', 13), width=23, height=2)
+            self.button3.place(x=265, y=240)
         else:
             tk.messagebox.showerror(title="Error", message="Файл пустой")
             app = Main()
@@ -163,7 +165,8 @@ class Classified_urls(tk.Toplevel):
         try:
             os.remove('classified_urls.csv')
             self.table.configure(state=tk.NORMAL)
-            self.table.delete('1.0', tk.END)
+            # Очистка всего содержимого таблицы кроме заголовков
+            self.table.delete('3.0', tk.END)
             self.table.configure(state=tk.DISABLED)
         except OSError:
             tk.messagebox.showerror(title="Error", message="Файл и так пустой")
